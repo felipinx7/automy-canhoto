5afc97e4f84106fbd63b9aba4ab4108e45f9738e
Felipe Dev <flimalimafelipek@gmail.com>
2026-06-08 12:27:53 -0300

    feat: initialization aplication

---
+++ baixa_canhotos.py
@@ -0,0 +1,11 @@
+#importação das blibliotecas
+import pyautogui
+import time
+
+#lógica da automação.
+pyautogui.press("win")
+time.sleep(2)
+
+
+pyautogui.write("chrome")
+pyautogui.press("enter")
---
+++ pegar_posicao.py
@@ -0,0 +1,6 @@
+# importação das blibliotecas
+import pyautogui
+import time
+
+#pegando o valor exato do click
+print(pyautogui.position())
