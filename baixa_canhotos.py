b77165b2ec0dbaaebd00449d9207d8df68fb94f4
Felipe Dev <flimalimafelipek@gmail.com>
2026-06-09 08:24:37 -0300

    feat: added planilha excel in code for reading data

--- baixa_canhotos.py
+++ baixa_canhotos.py
@@ -1,11 +1,22 @@
 #importação das blibliotecas
 import pyautogui
+import pandas as pd
 import time
 
-#lógica da automação.
-pyautogui.press("win")
-time.sleep(2)
+planilha_canhoto = pd.read_excel("Pasta1.xlsx")
+metodo_de_pagamento = planilha_canhoto["METODO DE PAGAMENTO"]
+codigo_canhoto = planilha_canhoto["CODIGO"]
 
+for i in range(len(planilha_canhoto)):
+    metodo = str(metodo_de_pagamento[i]).upper()
+    codigo = codigo_canhoto[i]
 
-pyautogui.write("chrome")
-pyautogui.press("enter")
+    if metodo in ["CREDITO", "DEBITO"]:
+        print(codigo)
+
+# #lógica da automação.
+# pyautogui.press("win")
+# time.sleep(2)
+
+# pyautogui.write("chrome")
+# pyautogui.press("enter")
