70f9ed88585a38229e8f0e21f0b6d3720dc4a979
Felipe Dev <flimalimafelipek@gmail.com>
2026-06-09 12:36:02 -0300

    feat: incremetation of news funcionality

--- baixa_canhotos.py
+++ baixa_canhotos.py
@@ -3,20 +3,27 @@ import pyautogui
 import pandas as pd
 import time
 
-planilha_canhoto = pd.read_excel("Pasta1.xlsx")
+# lendo as planilhas de canhoto e produtos.
+planilha_canhoto = pd.read_excel("planilha_canhotos.xlsx")
+planilha_produtos = pd.read_excel("planilha_produtos.xlsx")
+
+# pegando dados das planilhas de canhoto.
 metodo_de_pagamento = planilha_canhoto["METODO DE PAGAMENTO"]
 codigo_canhoto = planilha_canhoto["CODIGO"]
+valor_canhoto = planilha_canhoto["VALOR"]
 
+# pegando dados da planinlha de produtos.
+preco_produto = planilha_produtos["VALOR PRODUTO"]
+qtd_estoque = planilha_produtos["QUANTIDADE ESTOUQE"]
+
+
+# cadastro dos canhotos automatico.
 for i in range(len(planilha_canhoto)):
+    pyautogui.press("f1");
+
+    # lendo os produtos disponveis para cadastro dos canhotos.
     metodo = str(metodo_de_pagamento[i]).upper()
     codigo = codigo_canhoto[i]
 
     if metodo in ["CREDITO", "DEBITO"]:
         print(codigo)
-
-# #lógica da automação.
-# pyautogui.press("win")
-# time.sleep(2)
-
-# pyautogui.write("chrome")
-# pyautogui.press("enter")
